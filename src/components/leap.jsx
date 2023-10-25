6e7d66bc1b9f62d62f1ed69cef5896f0837e2d2f
sumitbhardwajjj <135961691+sumitbhardwajjj@users.noreply.github.com>
2023-10-25 16:11:40 +0530

    five

--- src/components/leap.jsx
+++ src/components/leap.jsx
@@ -9,34 +9,31 @@ import Todo from "./Todo";
 const Leap = () => {
   const dispatch = useDispatch();
 
-  const [num,setnum]= useState('')
+  const [num, setnum] = useState("");
   const [input, setinput] = useState("");
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (num.length<4) {
-    alert("Invalid field")
-    }else if((0 == num % 4) && (0 != num % 100) || (0 == num % 400)){
+    if (num.length < 4) {
+      alert("Invalid field");
+    } else if ((0 == num % 4 && 0 != num % 100) || 0 == num % 400) {
       dispatch(Addto(`${num} is leap year`));
-    }
-    else{
+    } else {
       dispatch(Addto(`${num} is an Ordinary year`));
     }
   };
 
-  const handleForm =(e)=>{
+  const handleForm = (e) => {
     e.preventDefault();
-  if(input.length>0){
-    dispatch(add(input))
-  }else{
-    alert('Write Something')
-  }
-  }
+    if (input.length > 0) {
+      dispatch(add(input));
+    } else {
+      alert("Write Something");
+    }
+  };
 
   return (
     <div className="container">
-    
       <form onSubmit={handleSubmit} className="form">
         <div className="form-2">
           <input
@@ -47,12 +44,11 @@ const Leap = () => {
           />
           <button type="submit">Enter</button>
         </div>
-     
       </form>
-      <Remove />
-    
-    
-     <form onSubmit={handleForm} className="form">
+      <div>
+        <Remove />
+      </div>
+      <form onSubmit={handleForm} className="form">
         <div className="form-2">
           <input
             value={input}
@@ -62,11 +58,11 @@ const Leap = () => {
           />
           <button type="submit">Enter</button>
         </div>
-     
       </form>
-      <Todo />
-     </div>
-  
+      <div>
+        <Todo />
+      </div>
+    </div>
   );
 };
 
